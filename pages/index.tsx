d5f79cf97b4192fd0a587b31dc79e3b10222989f
Lynn <lindell870@gmail.com>
2023-01-10 07:47:47 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { signIn } from 'next-auth/react'
-import Navbar from '../components/navbar';
+import Navbar from '../components/exnavbar';
 import Layout from '../components/welcome';
 import Footer from '../components/footer';
 
