ec5eafae25481776a9f3c4e9f1ce5b941b44e108
kaedenano <lindell870@gmail.com>
2023-01-12 00:01:31 +0900

    update prisma scheme

---
+++ pages/api/index.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  /* 著者リストを取得 */
+  const authors = await prisma.author.findMany();
+  res.status(200).json(authors);
+}
