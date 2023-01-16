7d517ad5b37145bd645d10d8d7fe5a06b8d3c2e0
Lynn <lindell870@gmail.com>
2023-01-16 11:36:57 +0900

    update

--- pages/[uuid].tsx
+++ pages/[uuid].tsx
@@ -37,6 +37,27 @@ export const view = ({ data }: any) => {
                     <Image rounded='2xl' src={data.artist[6].images[0].url} />
                     <Image rounded='2xl' src={data.artist[7].images[0].url} />
                 </SimpleGrid>
+                <Text
+                    as='i'
+                    fontSize={'96'}
+                    color='white' >
+                    Top Tracks
+                </Text>
+
+                <SimpleGrid
+                    columns={2}
+                    spacing={10}
+                    my='5'
+                >
+                    <Image rounded='2xl' src={data.track[0].album.images[0].url} />
+                    <Image rounded='2xl' src={data.track[1].album.images[0].url} />
+                    <Image rounded='2xl' src={data.track[2].album.images[0].url} />
+                    <Image rounded='2xl' src={data.track[3].album.images[0].url} />
+                    <Image rounded='2xl' src={data.track[4].album.images[0].url} />
+                    <Image rounded='2xl' src={data.track[5].album.images[0].url} />
+                    <Image rounded='2xl' src={data.track[6].album.images[0].url} />
+                    <Image rounded='2xl' src={data.track[7].album.images[0].url} />
+                </SimpleGrid>
 
             </Container>
         </>
